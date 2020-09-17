8a80f5f3aed59ea2c461a8d8c7d049baf9696b9d
Gabriel Farias <farias.gabriel.alejandro@hotmail.com>
2020-09-17 20:48:30 -0300

    Initial commit

---
+++ CargaDatosV4/CargaDatos/Form1.cs
@@ -0,0 +1,73 @@
+﻿using Lib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CargaDatos
+{
+    public partial class frmInicio : Form
+    {
+        //private string[] Nombres = new string[1];
+        //private string[] Apellidos = new string[1];
+
+        private Lista nombres = new Lista();
+        private Lista apellidos = new Lista();
+        //int Indice = 0;
+
+        public frmInicio()
+        {
+            InitializeComponent();
+            //ListaDeNombres.Redimension();
+            //ListaDeApellidos.Redimension();
+        }
+
+        private void btCargar_Click(object sender, EventArgs e)
+        {
+            nombres.CargaDatos(txtNombre.Text);
+            apellidos.CargaDatos(txtApellido.Text);
+
+            //if (Indice < Nombres.Length)
+            //{
+            //    CargaDatos();
+            //}
+            //else
+            //{
+            //    //actualizo la dimensión de Nombres
+            //    Nombres = Redimension(Nombres);
+            //    Apellidos = Redimension(Apellidos);
+            //    CargaDatos();
+            //}
+            
+            lblSalida.Text = lblSalida.Text + "\r\n" + txtNombre.Text + " " + txtApellido.Text;
+
+            txtNombre.Focus();
+            txtNombre.SelectAll();
+        }
+
+        //private string[] Redimension(string[] Dato)
+        //{
+        //    string[] temp = new string[Dato.Length + 1];
+        //    for (int i = 0; i < Dato.Length; i++)
+        //    {
+        //        temp[i] = Dato[i];
+        //    }
+        //    Dato = temp;
+
+        //    return Dato;
+        //}
+
+        //private void CargaDatos()
+        //{
+        //    Nombres[Indice] = txtNombre.Text;
+        //    Apellidos[Indice] = txtApellido.Text;
+        //    Indice = Indice + 1;
+        //    lblSalida.Text = lblSalida.Text + "\r\n" + txtNombre.Text + " " + txtApellido.Text;
+        //}
+    }
+}
---
+++ EjemploWhile/EjemploWhile/EjemploWhile.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploWhile
+{
+    public partial class EjemploWhile : Form
+    {
+        public EjemploWhile()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_Click(object sender, EventArgs e)
+        {
+            Random Generador = new Random();
+            
+            int AnchoFormulario;
+            int AnchoLabel;
+            int AnchoBorde;
+            int X;
+            int Paso;
+            int Repeticion = 0;
+            int TotalRepeticion = 10;
+            lbl.Width = 106;
+            lbl.Height = 15;
+            do
+            {
+                AnchoFormulario = this.Width;
+                AnchoLabel = lbl.Width;
+                AnchoBorde = 0;
+                X = 0;
+                Paso = Generador.Next(1, 50);
+                Repeticion = Repeticion + 1;
+
+                while (X < (AnchoFormulario - AnchoBorde - AnchoLabel))
+                {
+                    lbl.Left = X;
+                    this.Refresh();
+                    X = X + Paso;
+                }
+
+                lbl.Width = Generador.Next(2, 300);
+                lbl.Height = Generador.Next(5, 50);
+               
+
+                //lbl.Width = System.Convert.ToInt32( 0.7 * lbl.Width);
+                //lbl.Height = lbl.Height * 2;
+
+            } while (Repeticion < TotalRepeticion);
+        }
+    }
+}
